204dae8eb77df2d6502cc871dcea0d5ba3ddf723
Cornelius Denninger <cornelius.denninger@gmail.com>
2022-10-10 10:21:03 +0200

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head'
 
 import '@/styles/globals.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
@@ -9,6 +10,9 @@ import Layout from '@/components/layout/Layout';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <script async src="https://analytics.codenn.de/tracker.js" data-ackee-server="https://analytics.codenn.de" data-ackee-domain-id="2257f02c-01a9-4376-96b7-a3bc84e3eaa4"></script>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
