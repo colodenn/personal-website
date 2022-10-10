b7b5315a0902c748a05986ae566b2c11a222b64c
Cornelius Denninger <cornelius.denninger@gmail.com>
2022-10-10 20:48:53 +0200

    test2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,12 +10,12 @@ import Layout from '@/components/layout/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Head>
-        <Script async src="https://analytics.codenn.de/tracker.js" data-ackee-server="https://analytics.codenn.de" data-ackee-domain-id="2257f02c-01a9-4376-96b7-a3bc84e3eaa4"></Script>
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script async src="https://analytics.codenn.de/tracker.js" data-ackee-server="https://analytics.codenn.de" data-ackee-domain-id="2257f02c-01a9-4376-96b7-a3bc84e3eaa4"></Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
